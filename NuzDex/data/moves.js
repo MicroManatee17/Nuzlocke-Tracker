b2ab48c37c732a177fea179729f72552f4432737
MicroManatee17 <lmnsports@gmail.com>
2026-01-26 20:03:52 -0700

    Delete NuzDex directory to fix links

--- NuzDex/data/moves.js
+++
@@ -1,6 +0,0 @@
-window.MOVES_LIST = [
-  { name: "Tackle", type: "Normal", power: 40, accuracy: 100 },
-  { name: "Ember", type: "Fire", power: 40, accuracy: 100 },
-  { name: "Water Gun", type: "Water", power: 40, accuracy: 100 },
-  { name: "Vine Whip", type: "Grass", power: 45, accuracy: 100 }
-];
--- NuzDex/data/routes.js
+++
@@ -1,6 +0,0 @@
-window.ROUTE_LIST = [
-  "Pallet Town",
-  "Route 1",
-  "Viridian Forest",
-  "Pewter City"
-];
